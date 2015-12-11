0c93d04ad28d92113cc0c87118ce43691aeca987
Oliver Faircliff <faircliff.oliver@gmail.com>
2015-12-11 14:01:30 +0000

    Really golfed day 11

--- day11.py
+++ day11.py
@@ -1,29 +1,17 @@
 def parta(i):
     i = list(i)
     while 1:
-        for j in range(len(i)-1, -1, -1):
-            i[j] = chr(ord(i[j]) + 1)
-            if i[j] > 'z':
-                i[j] = 'a'
-            else:
-                break
-        r = d = p = s = a = 0
-        for l in i:
-            t = s
-            s = p
-            p = ord(l)
-            if s == p-1 and t == s-1:
-                r = 1
-            if a == p:
-                d += 1
-                a = 0
-            else:
-                a = p
-        if not any(l in i for l in 'iol') and r and d > 1:
-            break
-    global b
-    b = ''.join(i)
-    return b
+        r=d=p=s=a=c=0
+        for j in range(len(i)-1,-1,-1):
+            i[j] = (chr(ord(i[j])+1),i[j])[c];t=s;s=p
+            if i[j]>'z':i[j]='a'
+            else:c=1
+            p=ord(i[j])
+            if s==p+1 and t==s+1:r=1
+            if a==p:d+=1;a=0
+            else:a=p
+        if all(l not in i for l in 'iol')and r and d>1:break
+    global b;b=''.join(i);return b
 
 
 def partb(i):
